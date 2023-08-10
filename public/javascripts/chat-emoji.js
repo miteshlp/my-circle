ef7015207515ff15eee352ab3d5267c6c3295862
mitesh.p <mitesh.p@webcodegenie.com>
2023-08-10 18:56:46 +0530

    emoji blink fixed

--- public/javascripts/chat-emoji.js
+++ public/javascripts/chat-emoji.js
@@ -1,24 +1,27 @@
-$(document).on("click", "#emoji-picker", function (e) {
-    e.stopPropagation();
-    $('.intercom-composer-emoji-popover').toggleClass("active");
-});
+$(document).ready(function () {
 
-$(document).click(function (e) {
-    if ($(e.target).attr('class') != '.intercom-composer-emoji-popover' && $(e.target).parents(".intercom-composer-emoji-popover").length == 0) {
-        $(".intercom-composer-emoji-popover").removeClass("active");
-    }
-});
+    $(document).on("click", "#emoji-picker", function (e) {
+        e.stopPropagation();
+        $('.intercom-composer-emoji-popover').toggleClass("active");
+    });
 
-$(document).on("click", ".intercom-emoji-picker-emoji", function (e) {
-    $("#chat-message-box").val($("#chat-message-box").val() + $(this).html());
-});
+    $(document).click(function (e) {
+        if ($(e.target).attr('class') != '.intercom-composer-emoji-popover' && $(e.target).parents(".intercom-composer-emoji-popover").length == 0) {
+            $(".intercom-composer-emoji-popover").removeClass("active");
+        }
+    });
 
-$('.intercom-composer-popover-input').on('input', function () {
-    var query = this.value;
-    if (query != "") {
-        $(".intercom-emoji-picker-emoji:not([title*='" + query + "'])").hide();
-    }
-    else {
-        $(".intercom-emoji-picker-emoji").show();
-    }
-});
+    $(document).on("click", ".intercom-emoji-picker-emoji", function (e) {
+        $("#chat-message-box").val($("#chat-message-box").val() + $(this).html());
+    });
+
+    $('.intercom-composer-popover-input').on('input', function () {
+        var query = this.value;
+        if (query != "") {
+            $(".intercom-emoji-picker-emoji:not([title*='" + query + "'])").hide();
+        }
+        else {
+            $(".intercom-emoji-picker-emoji").show();
+        }
+    });
+})
